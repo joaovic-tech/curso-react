dd627f11fc2160aceef6d6c5a130d344a35cc97e
joaovic <joaovictorca2004@gmail.com>
2023-06-29 10:35:26 -0300

    aula 20-21

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,9 @@
+export const PostCard = ({ title, body, cover, id }) => (
+  <div className="post">
+    <img src={cover} alt={title} />
+    <div className="post-content">
+      <h2>{title}</h2>
+      <p>{body}</p>
+    </div>
+  </div>
+)
---
+++ src/utils/load-posts.js
@@ -0,0 +1,19 @@
+export const loadPosts = async () => {
+  const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+  const photosREsponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+
+  const [posts, photos] = await Promise.all([postsResponse, photosREsponse]);
+
+  const postJson = await posts.json();
+  const photosJson = await photos.json();
+
+  const postsAndPhotos = postJson.map((post, index) => {
+    return {
+      ...post,
+      cover: photosJson[index].url
+    }
+  });
+
+  return postsAndPhotos;
+}
